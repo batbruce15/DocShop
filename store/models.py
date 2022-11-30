285f067fb5baff8b092d1c4f1b78e04feb9050e2
batbruce15 <karim.battou20@gmail.com>
2022-11-30 10:29:52 +0100

    creation des modèle de produits

--- store/models.py
+++ store/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+"""
+Product (planification du modéle )
+- nom
+- prix
+- quantité en stock
+- description 
+- image
+"""
+
+
+class Product(models.Model):
+    nom = models.CharFeild(max_length=128)
+    price = models.FloatFeild(default=0.0)
+    stock = models.IntegerFeild(default=0)
+    description = models.TextFeild(blank=True)
+    thumbnail = models.ImageFeilf(Upload_to="products", blank=True, null=True)
